d5348ea67d9fa42b05df8b44f3cf359b7ebe07cb
wjdgh4058 <ljhmoa@gmail.com>
2022-07-27 17:36:00 +0900

    7/27/dicegame

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Dice from "./Dice";
+
+function App(){
+    return (
+    <div>
+        <Dice />
+    </div>);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,11 @@
-/*import { Fragment } from 'react';
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
-
-const product = 'MacBook';
-const model = 'Air';
-const item = product + model;
-
-function handleClick(){
-  alert('곧 도착합니다!');
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<>
-  <h1>나만의 {item} 주문하기</h1>
-  <button onClick={handleClick}>확인</button>
-</>
+<App />
 );
-*/
 
 
-import ReactDOM from 'react-dom';
 
-const WINS = {
-  rock: 'scissor',
-  scissor: 'paper',
-  paper: 'rock',
-};
-
-function getResult(left, right) {
-  if (WINS[left] === right) return '승리';
-  else if (left === WINS[right]) return '패배';
-  return '무승부';
-}
-
-function handleClick() {
-  console.log('가위바위보!');
-}
-
-const me = 'rock';
-const other = 'scissor';
-const result = getResult(me, other);
-ReactDOM.render(
-  <>
-    <h1>가위바위보</h1>
-    <h2>{result}</h2>
-    <button onClick ={handleClick}>가위</button>
-    <button onClick ={handleClick}>바위</button>
-    <button onClick ={handleClick}>보</button>
-  </>,
-  document.getElementById('root')
-);
