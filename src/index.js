dc9c2da2c19fd96d6766796daac3c079a99cc96b
wjdgh4058 <ljhmoa@gmail.com>
2022-07-27 16:02:13 +0900

    7/27/dicegame

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+/*import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const product = 'MacBook';
@@ -16,5 +16,37 @@ root.render(
   <button onClick={handleClick}>확인</button>
 </>
 );
+*/
 
 
+import ReactDOM from 'react-dom';
+
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
+const result = getResult(me, other);
+ReactDOM.render(
+  <>
+    <h1>가위바위보</h1>
+    <h2>{result}</h2>
+    <button onClick ={handleClick}>가위</button>
+    <button onClick ={handleClick}>바위</button>
+    <button onClick ={handleClick}>보</button>
+  </>,
+  document.getElementById('root')
+);
